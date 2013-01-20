94c1021d374015ca50392e4d9111c849ba259b3e
Stefan Ferstl <st.ferstl@gmail.com>
2013-01-20 14:02:41 +0100

    add parameterized test

--- src/test/java/com/github/ferstl/junitexamples/theory/ParameterizedTest.java
+++ src/test/java/com/github/ferstl/junitexamples/theory/ParameterizedTest.java
@@ -1,20 +1,50 @@
 package com.github.ferstl.junitexamples.theory;
 
-import org.junit.Before;
+import java.util.ArrayList;
+import java.util.Collection;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
 
-
+@RunWith(Parameterized.class)
 public class ParameterizedTest {
 
-  @Before
-  public void setUp() throws Exception {
+  @Parameters(name = "{0}{1}{2} -> {3}")
+  public static Iterable<Object[]> data() {
+    Collection<Object[]> data = new ArrayList<>(512);
+    for (Permission user : Permission.values()) {
+      for (Permission group : Permission.values()) {
+        for (Permission other : Permission.values()) {
+          int expected = 100 * user.iPermission + 10 * group.iPermission + other.iPermission;
+          data.add(new Object[] {user.sPermission, group.sPermission, other.sPermission, expected});
+        }
+      }
+    }
+    return data;
   }
 
+  private final String userPermission;
+  private final String groupPermission;
+  private final String otherPermission;
+  private final int expected;
+
+
+  public ParameterizedTest(String userPermission, String groupPermission, String otherPermission, int expected) {
+    this.userPermission = userPermission;
+    this.groupPermission = groupPermission;
+    this.otherPermission = otherPermission;
+    this.expected = expected;
+  }
+
+
   @Test
-  public void test() {
-    fail("Not yet implemented");
+  public void fromString() {
+    int permission = UnixFilePermissions.fromString(this.userPermission + this.groupPermission + this.otherPermission);
+    assertEquals(this.expected, permission);
   }
 
 }
